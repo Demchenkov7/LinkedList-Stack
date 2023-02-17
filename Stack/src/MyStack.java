ec45f621667e6fbf9f337e2412bd1c8d8ce3f0ec
Demchenkov7 <andreydemchenkov@yandex.ru>
2023-02-17 16:51:39 +0300

    first

---
+++ Stack/src/MyStack.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyStack {
+    private final List<String> storage = new LinkedList<>();
+
+    public void push(String s) {
+        storage.add(0, s);
+    }
+
+    public String pop() {
+        String element = storage.get(0);
+        storage.remove(0);
+        return element;
+    }
+
+    public String peek() {
+        return storage.get(0);
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public int search(String s) {
+        return storage.indexOf(s);
+    }
+}
